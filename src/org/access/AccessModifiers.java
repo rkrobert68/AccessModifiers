59e49afcd59c07145391d3652dcbda325eacf30b
rkrobert68 <rkrobert78@gmail.com>
2021-07-26 10:59:13 +0530

    this will work

---
+++ src/org/access/AccessModifiers.java
@@ -0,0 +1,13 @@
+package org.access;
+
+public class AccessModifiers {
+static int a;
+	private void name() {
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(a);
+	}
+	
+}
